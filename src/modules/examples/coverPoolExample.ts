384ab3234f566f25ac16cc2e0b745e626856f097
Amnot <acyclicgraphs@gmail.com>
2023-04-27 06:45:39 -0400

    Create coverPoolExample.ts

---
+++ src/modules/examples/coverPoolExample.ts
@@ -0,0 +1,28 @@
+import { ethers } from 'ethers';
+import { CoverPool, CoverPoolParams, MintParams } from './CoverPool';
+
+// Provide the contract address and Ethereum provider
+const coverPoolAddress = '0x...'; // Address of the deployed CoverPool contract
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545'); // JSON-RPC provider
+
+// Instantiate the CoverPool class
+const coverPool = new CoverPool(coverPoolAddress, provider);
+
+// Define parameters for the mint function
+const mintParams: MintParams = {
+  to: '0x...',
+  lower: 10,
+  upper: 20,
+  amount: 100,
+  claim: 15,
+  zeroForOne: true,
+};
+
+// Call the mint function
+coverPool.mint(mintParams).then((transaction) => {
+  console.log('Mint transaction:', transaction);
+}).catch((error) => {
+  console.error('Error calling mint:', error);
+});
+
+// ... Similarly, you can interact with other functions in the CoverPool contract
