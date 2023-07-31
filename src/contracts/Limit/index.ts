a488f07c6eea188b10be09bcedb36a896246d300
simonbindefeld <simonnfts@gmail.com>
2023-07-31 14:36:44 -0400

    feat: added Limit class and added index.ts in src

--- src/contracts/Limit/index.ts
+++ src/contracts/Limit/index.ts
@@ -1,25 +1,25 @@
-import { RangePoolSetters } from "./setters";
-import { RangePoolGetters } from "./getters";
+import { LimitPoolSetters } from "./setters";
+import { limitPoolGetters } from "./getters";
 import type {Provider} from '@ethersproject/providers';
 import { Network } from "../../enums";
 import { Signer } from "ethers";
 import { ContractConstructorArgs } from "../types";
 import { isSignerOrIsProvider,IsSignerOrIsProvider } from "../../utils/isSignerOrIsProvider";
 
-type RangePoolConstructor = ContractConstructorArgs; 
-export class RangePool {
-    public setters: RangePoolSetters | undefined;
-    public getters: RangePoolGetters;
+type LimitPoolConstructor = ContractConstructorArgs; 
+export class LimitPool {
+    public setters: LimitPoolSetters | undefined;
+    public getters: limitPoolGetters;
     public network: Network;
     public signerOrProvider: Signer | Provider;
     public debugMode: boolean;
     public poolAddress: `0x${string}`;
     
-    constructor(args:RangePoolConstructor) {
+    constructor(args:LimitPoolConstructor) {
         this.network = args.network ? args.network : Network.ARBITRUM;
         this.signerOrProvider = args.signerOrProvider;
 
-        this.getters = new RangePoolGetters({
+        this.getters = new limitPoolGetters({
             network: this.network,
             signerOrProvider: this.signerOrProvider,
             debugMode: args.debugMode,
@@ -33,7 +33,7 @@ export class RangePool {
         if(this.debugMode) console.log("Start CoverPool.initSetters")
         const isSignerOrIsProviderResult = await isSignerOrIsProvider(this.signerOrProvider);
         if(isSignerOrIsProviderResult === IsSignerOrIsProvider.Signer) {
-            this.setters = new RangePoolSetters({
+            this.setters = new LimitPoolSetters({
                 network: this.network,
                 signerOrProvider: this.signerOrProvider as Signer,
                 debugMode: false,
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./contracts/Cover";
+export * from "./contracts/Range";
+export * from "./contracts/Limit";
+export * from "./types/types";
+export * from "./subgraph"
